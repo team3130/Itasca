07328d2ab8c4f403d930c0888e49944e55be6a79
Tantan4321 <kupa528@yahoo.com>
2018-01-08 20:20:13 -0600

    Added userLibs and constants managing system

---
+++ src/org/usfirst/frc/team3130/robot/Constants.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team3130.robot;
+
+import org.usfirst.frc.team3130.util.ConstantsBase;
+
+import edu.wpi.first.wpilibj.Preferences;
+
+public class Constants extends ConstantsBase {
+	
+}
