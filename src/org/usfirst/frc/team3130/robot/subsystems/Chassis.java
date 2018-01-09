bc9ba4d040b874c28db9d6cb12c0f275e3e6bb8f
Ashley Holton <ashleyholton2@hotmail.com>
2018-01-08 20:25:04 -0600

    Created base subsystems

---
+++ src/org/usfirst/frc/team3130/robot/subsystems/Chassis.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team3130.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+
+/**
+ *
+ */
+public class Chassis extends PIDSubsystem {
+
+    // Initialize your subsystem here
+    public Chassis() {
+        // Use these to get going:
+        // setSetpoint() -  Sets where the PID controller should move the system
+        //                  to
+        // enable() - Enables the PID controller.
+    	super(1.0,0,0);
+    }
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+
+    protected double returnPIDInput() {
+        // Return your input value for the PID loop
+        // e.g. a sensor, like a potentiometer:
+        // yourPot.getAverageVoltage() / kYourMaxVoltage;
+        return 0.0;
+    }
+
+    protected void usePIDOutput(double output) {
+        // Use output to drive your system, like a motor
+        // e.g. yourMotor.set(output);
+    }
+}
