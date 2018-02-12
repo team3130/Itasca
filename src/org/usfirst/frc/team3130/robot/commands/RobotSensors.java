bc397e67b6b21fd355a3618d9d6438c39c262390
Mikhail Kyraha <mikhail@kyraha.com>
2018-02-11 18:28:17 -0600

    Working but dirty range finder. To be cleaned up

--- src/org/usfirst/frc/team3130/robot/commands/RobotSensors.java
+++ src/org/usfirst/frc/team3130/robot/commands/RobotSensors.java
@@ -37,7 +37,14 @@ public class RobotSensors extends Command {
     	//Elevator
     	Elevator.outputToSmartDashboard();
 
-    	SmartDashboard.putNumber("Lidar range", Rangefinder.getDistance());
+    	Rangefinder rf = Rangefinder.GetInstance();
+    	int distance = Rangefinder.getDistance();
+    	if(distance>=0) {
+    		SmartDashboard.putNumber("Lidar range", distance);
+    	}
+    	
+    	SmartDashboard.putBoolean("LIDAR Ready", rf.getDistanceReady());
+    	SmartDashboard.putNumber("LIDAR status", rf.getDistanceStatus());
 
     	//SmartDashboard.putNumber("Left Index Current", Robot.btLeftIndex.getCurrent());
     	//SmartDashboard.putNumber("Right Index Current", Robot.btRightIndex.getCurrent());
--- src/org/usfirst/frc/team3130/robot/sensors/Rangefinder.java
+++ src/org/usfirst/frc/team3130/robot/sensors/Rangefinder.java
@@ -12,7 +12,7 @@ public class Rangefinder {
 	private final int LIDAR_ADDR = 0x52>>1;
 
 	//Instance Handling
-	private static Rangefinder m_pInstance;
+	private static Rangefinder m_pInstance = null;
 	public static Rangefinder GetInstance()
 	{
 		if(m_pInstance == null) m_pInstance = new Rangefinder();
@@ -22,67 +22,149 @@ public class Rangefinder {
 	public Rangefinder() {
 		i2c = new I2C(I2C.Port.kOnboard, LIDAR_ADDR);
 		// Mandatory : private registers
-		i2c.write(0x0207, 0x01);
-		i2c.write(0x0208, 0x01);
-		i2c.write(0x0096, 0x00);
-		i2c.write(0x0097, 0xfd);
-		i2c.write(0x00e3, 0x00);
-		i2c.write(0x00e4, 0x04);
-		i2c.write(0x00e5, 0x02);
-		i2c.write(0x00e6, 0x01);
-		i2c.write(0x00e7, 0x03);
-		i2c.write(0x00f5, 0x02);
-		i2c.write(0x00d9, 0x05);
-		i2c.write(0x00db, 0xce);
-		i2c.write(0x00dc, 0x03);
-		i2c.write(0x00dd, 0xf8);
-		i2c.write(0x009f, 0x00);
-		i2c.write(0x00a3, 0x3c);
-		i2c.write(0x00b7, 0x00);
-		i2c.write(0x00bb, 0x3c);
-		i2c.write(0x00b2, 0x09);
-		i2c.write(0x00ca, 0x09);
-		i2c.write(0x0198, 0x01);
-		i2c.write(0x01b0, 0x17);
-		i2c.write(0x01ad, 0x00);
-		i2c.write(0x00ff, 0x05);
-		i2c.write(0x0100, 0x05);
-		i2c.write(0x0199, 0x05);
-		i2c.write(0x01a6, 0x1b);
-		i2c.write(0x01ac, 0x3e);
-		i2c.write(0x01a7, 0x1f);
-		i2c.write(0x0030, 0x00);
+		System.out.println("LIDAR Init Started");
+		writeByte(0x0207, 0x01);
+		System.out.println("LIDAR Complete 1 Byte Transfer");
+
+		writeByte(0x0208, 0x01);
+		writeByte(0x0096, 0x00);
+		writeByte(0x0097, 0xfd);
+		writeByte(0x00e3, 0x00);
+		writeByte(0x00e4, 0x04);
+		writeByte(0x00e5, 0x02);
+		writeByte(0x00e6, 0x01);
+		writeByte(0x00e7, 0x03);
+		writeByte(0x00f5, 0x02);
+		writeByte(0x00d9, 0x05);
+		writeByte(0x00db, 0xce);
+		writeByte(0x00dc, 0x03);
+		writeByte(0x00dd, 0xf8);
+		writeByte(0x009f, 0x00);
+		writeByte(0x00a3, 0x3c);
+		writeByte(0x00b7, 0x00);
+		writeByte(0x00bb, 0x3c);
+		writeByte(0x00b2, 0x09);
+		writeByte(0x00ca, 0x09);
+		writeByte(0x0198, 0x01);
+		writeByte(0x01b0, 0x17);
+		writeByte(0x01ad, 0x00);
+		writeByte(0x00ff, 0x05);
+		writeByte(0x0100, 0x05);
+		writeByte(0x0199, 0x05);
+		writeByte(0x01a6, 0x1b);
+		writeByte(0x01ac, 0x3e);
+		writeByte(0x01a7, 0x1f);
+		writeByte(0x0030, 0x00);
 
 		// Recommended : Public registers - See data sheet for more detail
 		 // Enables polling for ‘New Sample ready’ when measurement completes
-		i2c.write (0x0011 , 0x10);
+		writeByte (0x0011 , 0x10);
 		 // Set the averaging sample period
 		 // (compromise between lower noise and increased execution time)
-		i2c.write (0x010a , 0x30);
+		writeByte (0x010a , 0x30);
 		 // Sets the light and dark gain (upper nibble). Dark gain should not be changed.
-		i2c.write (0x003f , 0x46);
+		writeByte (0x003f , 0x46);
 		 // sets the # of range measurements after which auto calibration of system is performed   
-		i2c.write (0x0031 , 0xFF);
+		writeByte (0x0031 , 0xFF);
 		 // Set ALS integration time to 100ms
-		i2c.write (0x0040 , 0x63);
+		writeByte (0x0040 , 0x63);
 		 // perform a single temperature calibration of the ranging sensor 
-		i2c.write (0x002e , 0x01);
+		writeByte (0x002e , 0x01);
 
 		 //Start Continuous Ranging
-		i2c.write(0x0018, 0x03);
+		writeByte(0x0018, 0x01);
+		System.out.println("LIDAR Init complete");
 	}
 
 	// Distance in mm
 	public static int getDistance() {
 		Rangefinder instance = GetInstance();
+		int reg = 0x0062;
 		byte[] buffer = new byte[1];
-		if( !instance.i2c.read(0x62, 1, buffer) ) {
-			return buffer[0];
+		/*
+		if (!instance.getDistanceReady()) {
+			return -1;
+		}*/
+		
+		byte[] wbuffer = new byte[2];
+		
+		wbuffer[0] = (byte) ( (reg>>8) &0xFF);
+		wbuffer[1] = (byte) (reg &0xFF);
+
+		if(!instance.i2c.writeBulk(wbuffer)) {				
+			if( !instance.i2c.readOnly(buffer, 1) ) {
+				int num = buffer[0];
+				if(num < 0) {
+					num = num&127 + 128;
+				}
+				instance.writeByte(0x0018, 0x01);
+				
+				return num;
+			}
+			else {
+				return -1;
+			}
 		}
 		else {
 			return -1;
 		}
 	}
 	
+	// RESULT_RANGE_STATUS register
+		public int getDistanceStatus() {
+			byte[] buffer = new byte[1];
+			if( !i2c.read(0x4D, 1, buffer) ) {
+				int num = (buffer[0]>>4)&0x0F;
+				return num;
+			}
+			else {
+				return -1;
+			}
+		}
+		
+		// Range Ready register
+			public boolean getDistanceReady() {
+				int reg = 0x004F;
+				byte[] buffer = new byte[1];
+				
+				byte[] wbuffer = new byte[2];
+				wbuffer[0] = (byte) ( (reg>>8) &0xFF);
+				wbuffer[1] = (byte) (reg &0xFF);
+
+				if(!i2c.writeBulk(wbuffer)) {
+					if( !i2c.readOnly(buffer, 1)) {
+						int num = buffer[0]&0x04;
+						return num > 0;
+					}
+					else {
+						return false;
+					}
+				}
+				else {
+					System.out.println("Failed to Write Bulk in get Distance Ready");
+					return false;
+				}
+			}	
+	private boolean writeByte(int reg, int data) {
+		byte[] buffer = new byte[3];
+		
+		buffer[0] = (byte) ( (reg>>8) & 0xFF);
+		buffer[1] = (byte) (reg & 0xFF);
+		buffer[2] = (byte) ((byte) data & 0xFF);
+		
+		byte count = 0;
+		while(count<3) {
+			if(!i2c.writeBulk(buffer)) {
+				return true;
+			}
+			else {
+				count ++;
+			}
+			System.out.println("LIDAR Write Byte Failed " + count + " times.");
+
+			
+		}
+		return false;
+	}
 }
 
