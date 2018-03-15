cb768d0cc258afe8de62fb4e95d3f42f7ce9934f
Duckfan77 <cwenthin@yahoo.com>
2018-03-14 21:22:25 -0500

    Redid SwitchSide and tuned

--- src/org/usfirst/frc/team3130/robot/autoCommands/SwitchSide.java
+++ src/org/usfirst/frc/team3130/robot/autoCommands/SwitchSide.java
@@ -1,14 +1,11 @@
 package org.usfirst.frc.team3130.robot.autoCommands;
 
-import org.usfirst.frc.team3130.robot.Constants;
 import org.usfirst.frc.team3130.robot.commands.ElevatorToHeight;
+import org.usfirst.frc.team3130.robot.commands.RunIntakeIn;
+import org.usfirst.frc.team3130.robot.commands.RunIntakeOut;
 import org.usfirst.frc.team3130.robot.subsystems.Chassis;
 import org.usfirst.frc.team3130.robot.subsystems.CubeIntake;
 import org.usfirst.frc.team3130.robot.subsystems.Elevator;
-import org.usfirst.frc.team3130.robot.commands.RunIntakeIn;
-import org.usfirst.frc.team3130.robot.commands.RunIntakeOut;
-import org.usfirst.frc.team3130.robot.continuousDrive.ContDrive;
-import org.usfirst.frc.team3130.robot.continuousDrive.ContTurnDist;
 
 import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -17,63 +14,60 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *  Deposit cube into switch from the side
  */
 public class SwitchSide extends CommandGroup {
-
-	private AutoDelay				 delay;
-	private ContDrive                driveForward;
-	private RunIntakeIn	             intakeIn;
-	private ContTurnDist      		 turnToSwitch;
-	private ContDrive				 toSwitch;
-	private ElevatorToHeight		 elevatorUp;
-	private RunIntakeOut			 intakeOut;
-	private ContTurnDist      		 turnDownField;
-	private ContDrive                driveDownField;
-	private char					 side;
 	
-    public SwitchSide(char side) {
-    	requires(Chassis.GetInstance());
-    	requires(CubeIntake.GetInstance());
-    	requires(Elevator.GetInstance());
-    	
-    	this.side      = side;
-    	delay		   = new AutoDelay();
-    	driveForward   = new ContDrive();
-    	intakeIn  	   = new RunIntakeIn();
-    	turnToSwitch   = new ContTurnDist();
-    	//toSwitch 	   = new ContDrive();
-    	elevatorUp 	   = new ElevatorToHeight(35.0);
-    	intakeOut 	   = new RunIntakeOut();
-    	turnDownField  = new ContTurnDist();
-    	driveDownField   = new ContDrive();
-    	
-    	//addParallel(intakeIn);
-    	addParallel(elevatorUp);
-    	addSequential(driveForward, 3);
-    	addSequential(turnToSwitch, 1);
-    	//addSequential(toSwitch, 2);
-    	addSequential(delay, 1);
-    	addSequential(intakeOut, 1);
-    	addSequential(turnDownField, 1);
-    	addSequential(driveDownField, 2);
-    	
-    }
-    
-    @Override
-    protected void initialize(){
-    	intakeIn.SetParam(0.3);
-    	driveForward.SetParam(
-            Preferences.getInstance().getDouble("AUTON Forward Speed", 0.7), 
-    		Constants.kWallToSwitch - (Constants.kChassisLength / 2.0),
-    		false
-        );
-    	if(side == 'L'){
-    		turnToSwitch.SetParam(0.31, 90*(Math.PI/180f), false);
-        	turnDownField.SetParam(-0.6, 90*(Math.PI/180f), false);
-    	}
-    	else{
-    		turnToSwitch.SetParam(0.31, -90*(Math.PI/180f), false);
-        	turnDownField.SetParam(-0.6, -90*(Math.PI/180f), false);
-    	}
-    	intakeOut.SetParam(-0.4);
-    	driveDownField.SetParam(0.7, 70.0, false);
-    }
+	private AutoDriveStraightToPoint	driveForward;
+	private AutoDriveStraightToPoint	driveToSwitch;
+	private AutoDriveStraightToPoint	driveBack;
+	private AutoTurn					turnToSwitch;
+	private ElevatorToHeight			elevatorUp;
+	private RunIntakeIn					intakeIn;
+	private RunIntakeOut				intakeOut;
+	private char						side;
+	
+	public SwitchSide(char side) {
+		requires(Chassis.GetInstance());
+		requires(CubeIntake.GetInstance());
+		requires(Elevator.GetInstance());
+		
+		this.side		= side;
+		driveForward	= new AutoDriveStraightToPoint();
+		driveToSwitch	= new AutoDriveStraightToPoint();
+		driveBack		= new AutoDriveStraightToPoint();
+		turnToSwitch	= new AutoTurn();
+		elevatorUp		= new ElevatorToHeight(0);
+		intakeIn		= new RunIntakeIn();
+		intakeOut		= new RunIntakeOut();
+		
+		addParallel(intakeIn, 1);
+		addSequential(driveForward, 3);
+		addSequential(elevatorUp, 3);
+		addSequential(turnToSwitch, 2);
+		addSequential(driveToSwitch, 2);
+		addSequential(intakeOut, 1);
+		addSequential(driveBack, 2);
+	}
+	
+	@Override
+	protected void initialize(){
+		intakeIn.SetParam(0.3);
+		elevatorUp.setParam(32);
+		intakeOut.SetParam(-0.7);
+		driveForward.SetParam(
+				224, 
+				20, 
+				Preferences.getInstance().getDouble("ScaleForwardSpeed", .5), 
+				false
+			);
+		driveBack.SetParam(
+				-24, 
+				5, 
+				0.5, 
+				false);
+		driveToSwitch.SetParam(40, 10, 0.4, false);
+		if(side=='L'){
+			turnToSwitch.setParam(90, 5);
+		}else{
+			turnToSwitch.setParam(-95, 5);
+		}
+	}
 }
