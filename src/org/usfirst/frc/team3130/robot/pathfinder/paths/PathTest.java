da5e1f990fa13788d613cc1f5894ec37d9781b98
Tantan4321 <kupa528@yahoo.com>
2018-03-16 20:07:04 -0500

    Test path

---
+++ src/org/usfirst/frc/team3130/robot/pathfinder/paths/PathTest.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team3130.robot.pathfinder.paths;
+
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Waypoint;
+
+public class PathTest {
+	
+	public static Waypoint[]path = new Waypoint[] {
+			new Waypoint(0.0, 0.0, Pathfinder.d2r(0.0)),
+            new Waypoint(1.0, 1.0, Pathfinder.d2r(90.0)),
+	};
+}
