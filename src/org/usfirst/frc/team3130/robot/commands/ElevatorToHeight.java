186fb8cf8acbed3cce509482f47e279b6838d1cb
Ashley Holton <ashleyholton2@hotmail.com>
2018-03-27 19:57:06 -0500

    added interrupted() to ElevatorToHeight

--- src/org/usfirst/frc/team3130/robot/commands/ElevatorToHeight.java
+++ src/org/usfirst/frc/team3130/robot/commands/ElevatorToHeight.java
@@ -38,11 +38,12 @@ public class ElevatorToHeight extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	
+    	Elevator.holdHeight();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
