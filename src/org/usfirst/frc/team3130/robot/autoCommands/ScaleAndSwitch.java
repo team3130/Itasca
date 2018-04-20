64db5d708de2ea675455cb7a68a588a3d3d1296e
Ashley Holton <ashleyholton2@hotmail.com>
2018-04-20 18:30:46 -0500

    tuning scale and switch
    cross scale and then put in switch right behind now works

--- src/org/usfirst/frc/team3130/robot/autoCommands/ScaleAndSwitch.java
+++ src/org/usfirst/frc/team3130/robot/autoCommands/ScaleAndSwitch.java
@@ -23,6 +23,7 @@ public class ScaleAndSwitch extends CommandGroup {
 	private ElevatorToHeight			elevatorDown;
 	private ElevatorToHeight			eleDownAgain;
 	private ElevatorToHeight			eleUpAgain;
+	private ElevatorToHeight			eleUp;
 	private ElevatorToHeight			eleRaise;
 	private RunIntakeIn					intakeIn;
 	private RunIntakeOut				intakeOut;
@@ -50,6 +51,8 @@ public class ScaleAndSwitch extends CommandGroup {
 	private AutoDriveStraightToPoint	straightAgain;
 	private AutoDriveStraightToPoint	driveBehind;
 	private AutoDriveStraightToPoint	driveToScale;
+	private AutoDriveStraightToPoint	toCube;
+	private AutoDriveStraightToPoint	closer;
 	private AutoTurn					turnSecond;
 	private AutoTurn					turnLast;
 	private AutoTurn					turnBehind;
@@ -87,6 +90,7 @@ public class ScaleAndSwitch extends CommandGroup {
 		intakeCube   = new RunIntakeIn();
 		elevatorUpAgain = new ElevatorToHeight(40);
 		eleUpAgain = new ElevatorToHeight(40);
+		eleUp		 = new ElevatorToHeight(40);
 		eleRaise	 = new ElevatorToHeight(98);
 		depositCube  = new RunIntakeOut();
 		turnAway     = new AutoTurn();
@@ -109,14 +113,16 @@ public class ScaleAndSwitch extends CommandGroup {
 		driveToScale	 = new AutoDriveStraightToPoint();
 		turnBehind   = new AutoTurn();
 		driveBehind = new AutoDriveStraightToPoint();
+		toCube = new AutoDriveStraightToPoint();
+		closer = new AutoDriveStraightToPoint();
 		
 		//beginning of auton
 		addParallel(elevatorUp,3);
 		addParallel(intakeIn,1);
-		addSequential(driveForward,4.1);
 		
 		//scale is on same side as start
 		if(sameScale){
+			addSequential(driveForward,4.1);
 			addSequential(turnToScale, 1.1);
 			addSequential(intakeOut, 1);
 			//switch is on the same side as the scale
@@ -128,29 +134,31 @@ public class ScaleAndSwitch extends CommandGroup {
 				addSequential(openIntake, 0.5);
 				addSequential(turnToCube, 1.5);
 				addParallel(intakeCube, 3);
-				addSequential(driveToCube, 1.5);
+				addSequential(driveToCube, 0.75);
 				addSequential(closeIntake, 0.5);
 				addParallel(elevatorUpAgain, 1.5);
-				addSequential(delay3, 1.0);
+				addSequential(delay3, 0.5);
 				addSequential(toSwitch, 1);
 				addSequential(depositCube, 1);
 				addSequential(back, 1);
 				addParallel(eleDownAgain, 2);
 				addSequential(turnSecond, 1);
-				addParallel(openIntakeAgain, 1);
+				//addParallel(openIntakeAgain, 1);
 				addParallel(intakeAgain, 2.5);
 				addSequential(straight, 1.5);
-				addSequential(turnLast, 1);
-				addSequential(straightAgain, 0.5);
-				addSequential(closeIntakeAgain, 0.3);
 				addParallel(eleUpAgain, 2);
-				addSequential(toSwitchLast, 1);
+				addSequential(delay, 0.5);
+				addSequential(turnLast, 1);
+				//addSequential(straightAgain, 0.5);
+				//addSequential(closeIntakeAgain, 0.3);
+				//addSequential(toSwitchLast, 1);
 				addSequential(cubeSpit, 1);
 			}
 		}
 		//scale is on opposite side of start
 		else{
-			addSequential(turnBehind, 2);
+			addSequential(driveForward,2.0);
+			addSequential(turnBehind, 1.5);
 			addSequential(driveBehind,5);
 			addParallel(eleRaise,3);
 			addSequential(turnToScale,1);
@@ -159,17 +167,32 @@ public class ScaleAndSwitch extends CommandGroup {
 			addParallel(elevatorDown, 3);
 			addSequential(driveBack,2);
 			//switch is on the same side as the scale
-			/*
+			///*
 			if(sameSwitch){
 				addParallel(intakeCube, 4);
 				addParallel(openIntake, 0.5);
 				addSequential(turnToCube, 2);
-				addSequential()
+				addSequential(toCube, 2.0);
+				addSequential(delay2, 0.5);
+				addSequential(closeIntake, 0.5);
+				addParallel(eleUp, 3.0);
+				addSequential(closer, 2.0);
+				addSequential(depositCube, 1.0);
+				addSequential(back, 1);
+				addParallel(eleDownAgain, 2);
+				addSequential(turnSecond, 1);
+				//addParallel(openIntakeAgain, 1);
+				addParallel(intakeAgain, 4);
+				//addSequential(closeIntakeAgain, 0.5);
+				addSequential(straight, 3.5);
+				addParallel(eleUpAgain, 2);
+				addSequential(toSwitchLast, 1);
+				addSequential(cubeSpit, 1);
 			}
 			//switch is opposite the scale
 			else{
 				
-			}*/
+			}//*/
 		}
 	}
     
@@ -187,69 +210,15 @@ public class ScaleAndSwitch extends CommandGroup {
 		elevatorUpAgain.setParam(40.0);
 		eleUpAgain.setParam(40.0);
 		eleRaise.setParam(98);
-		if(sameScale){
-			elevatorUp.setParam(98);
-			intakeOut.SetParam(-1.0);
-			driveForward.SetParam(
-					Preferences.getInstance().getDouble("ScaleSwitch Forward Dist", 404), 
-					20, 
-					0.85, 
-					false
-			);
-			if(scale =='L'){		//Left Side Scale, Switch, and Start
-				turnToScale.setParam(45, 5);
-				turnAway.setParam(80, 3);
-				turnToCube.setParam(Preferences.getInstance().getDouble("ScaleSwitch turnToCube Left Side", 45), 2);
-				turnToSwitch.setParam(20, 3);
-				turnSecond.setParam(-45, 3);
-				turnLast.setParam(55, 3);
-			}else{				//Right Side Scale, Switch, and Start
-				turnToScale.setParam(-55, 2);
-				turnToCube.setParam(Preferences.getInstance().getDouble("ScaleSwitch turnToCube Right Side", -45), 2);
-				turnToSwitch.setParam(-20, 3);
-			}
-		}
-		else {
-			elevatorUp.setParam(12);
-			intakeOut.SetParam(-0.4);
-			driveForward.SetParam(
-					348, 
-					5, 
-					0.75, 
-					false
-			);
-			driveBehind.SetParam(
-					300, 
-					5, 
-					.75, 
-					false
-			);
-			driveToScale.SetParam(
-					24, 
-					10, 
-					0.6, 
-					false
-			);
-			if(scale == 'L'){
-				turnToScale.setParam(100, 1);
-				turnBehind.setParam(-90, 1);
-				turnToCube.setParam(-180, 3);
-			}
-			else{
-				turnToScale.setParam(-100, 1);
-				turnBehind.setParam(90, 1);
-				turnToCube.setParam(180, 3);
-			}
-		}
 		driveBack.SetParam(
-				Preferences.getInstance().getDouble("ScaleSwitch Back Dist", -24), 
+				Preferences.getInstance().getDouble("ScaleSwitch Back Dist", -22), 
 				5, 
-				0.5, 
+				0.55, 
 				false
 		);
 		driveToCube.SetParam(//106
-				Preferences.getInstance().getDouble("ScaleSwitch ToCube Dist", 30),  
-				5, 
+				Preferences.getInstance().getDouble("ScaleSwitch ToCube Dist", 40),  
+				3, 
 				.85, 
 				false
 		);
@@ -272,13 +241,13 @@ public class ScaleAndSwitch extends CommandGroup {
 				false
 		);
 		back.SetParam(
-				-36, 
+				-18, 
 				8, 
 				.7, 
 				false
 		);
 		straight.SetParam(
-				44, 
+				50, 
 				4, 
 				.7, 
 				false
@@ -295,7 +264,80 @@ public class ScaleAndSwitch extends CommandGroup {
 				.7, 
 				false
 		);
-		//driveToScale.SetParam(12, 10, 0.4, false);
-		
+		if(sameScale){
+			elevatorUp.setParam(98);
+			intakeOut.SetParam(-0.9);
+			driveForward.SetParam(
+					Preferences.getInstance().getDouble("ScaleSwitch Forward Dist", 388), //404
+					10, 
+					1.00, 
+					false
+			);
+			if(scale =='L'){		//Left Side Scale, Switch, and Start
+				turnToScale.setParam(45, 5);
+				turnAway.setParam(80, 3);
+				turnToCube.setParam(Preferences.getInstance().getDouble("ScaleSwitch turnToCube Left Side", 45), 2);
+				turnToSwitch.setParam(20, 3);
+				turnSecond.setParam(-45, 3);
+				turnLast.setParam(45, 3);
+			}else{				//Right Side Scale, Switch, and Start
+				turnToScale.setParam(-55, 2);
+				turnToCube.setParam(Preferences.getInstance().getDouble("ScaleSwitch turnToCube Right Side", -45), 2);
+				turnToSwitch.setParam(-20, 3);
+				turnSecond.setParam(60, 3);
+				turnLast.setParam(-75, 3);
+			}
+		}
+		else {
+			if(sameSwitch){
+				elevatorUp.setParam(12);
+				eleUp.setParam(40);
+				intakeOut.SetParam(-0.4);
+				driveForward.SetParam(
+						348, 
+						5, 
+						0.8, 
+						false
+				);
+				driveBehind.SetParam(
+						330, 
+						10, 
+						.7, 
+						false
+				);
+				driveToScale.SetParam(
+						36, 
+						10, 
+						0.6, 
+						false
+				);
+				toCube.SetParam(
+						95, 
+						3,
+						0.7, 
+						false
+				);
+				closer.SetParam(
+						8, 
+						2,
+						0.7, 
+						false
+				);
+				if(scale == 'L'){
+					turnToScale.setParam(110, 1);
+					turnBehind.setParam(-90, 1);
+					turnToCube.setParam(140, 3);
+					turnSecond.setParam(-75, 3);
+					turnLast.setParam(70, 3);
+				}
+				else{
+					turnToScale.setParam(-110, 1);
+					turnBehind.setParam(90, 1);
+					turnToCube.setParam(-140, 3);
+					turnSecond.setParam(75, 3);
+					turnLast.setParam(-70, 3);
+				}
+			}
+		}
     }
 }
